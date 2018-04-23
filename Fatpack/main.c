27f3fb4f705fcb895ea2ac052cd6a9466e018fb9
Sijmen J. Mulder <ik@sjmulder.nl>
2018-04-23 07:09:42 +0200

    Add dialog to Fatpack

--- Fatpack/main.c
+++ Fatpack/main.c
@@ -1,7 +1,47 @@
 #include <windows.h>
+#include <tchar.h>
+#include "../common/util.h"
+#include "resource.h"
+
+static INT_PTR CALLBACK
+dialogproc(HWND wnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg) {
+	case WM_CLOSE:
+		DestroyWindow(wnd);
+		return TRUE;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return TRUE;
+	}
+
+	return FALSE;
+}
 
 int CALLBACK
 WinMain(HINSTANCE instance, HINSTANCE prev, LPSTR cmdline, int cmdshow)
 {
+	HWND dialog;
+	MSG msg;
+	BOOL ret;
+
+	dialog = CreateDialog(instance, MAKEINTRESOURCE(IDD_FATPACK), NULL,
+	    dialogproc);
+	if (!dialog)
+		err(_T("CreateDialog"));
+
+	ShowWindow(dialog, cmdshow);
+
+	while (ret = GetMessage(&msg, NULL, 0, 0) > 0) {
+		if (!IsDialogMessage(dialog, &msg)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	if (ret == -1)
+		err(_T("GetMessage()"));
+
 	return 0;
 }
---
+++ Fatpack/resource.h
@@ -0,0 +1,10 @@
+#define IDR_LOADER	100
+
+#define IDD_FATPACK	200
+
+#define IDC_EXELIST	300
+#define IDC_ADD		301
+#define IDC_REMOVE	302
+#define IDC_MOVEUP	303
+#define IDC_MOVEDOWN	304
+#define IDC_PACK	305
